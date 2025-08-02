10ea89bb4886d9a28ed7929a373d62cd02df3d73
sameer-jafar-05 <sameer.jafar.05@gmail.com>
2025-08-03 01:41:59 +0530

    Create AboutSection.js

---
+++ components/AboutSection.js
@@ -0,0 +1,21 @@
+const AboutSection = () => {
+  return (
+    <section className="about section" id="about">
+      <div className="container">
+        <div className="section-header">
+          <h2>About Me</h2>
+          <p>A bit about my journey and passion</p>
+        </div>
+        <div className="about-content">
+          <div className="about-avatar profile-avatar">📘</div>
+          <div className="about-text">
+            <h3>BTech CSE Student | Social Entrepreneur</h3>
+            <p>
+              I'm a 4th-year Computer Science student at Vasavi College of Engineering. I founded "Amma Educate" to teach underprivileged children and also run an English-speaking academy. I enjoy solving problems and creating tools that help others grow.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
