e1d5f998aa8a1286de5f209cefe6260ada10ec0d
sameer-jafar-05 <sameer.jafar.05@gmail.com>
2025-08-03 02:09:27 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,10 +5,10 @@ const Navbar = () => {
         <div className="logo">Sameer</div>
         <nav>
           <ul className="nav-links">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="projects.html">Projects</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="#" onClick={() => window.location.href = 'index.html'}>Home</a></li>
+            <li><a href="#" onClick={() => window.location.href = 'about.html'}>About</a></li>
+            <li><a href="#" onClick={() => window.location.href = 'projects.html'}>Projects</a></li>
+            <li><a href="#" onClick={() => window.location.href = 'contact.html'}>Contact</a></li>
           </ul>
         </nav>
         <div className="mobile-menu">
