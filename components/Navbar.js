9bb2f8e08272ef9aa2ad67f2fbf337456b663d3f
sameer-jafar-05 <sameer.jafar.05@gmail.com>
2025-08-03 02:04:25 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,15 +2,15 @@ const Navbar = () => {
   return (
     <header className="navbar">
       <div className="container nav-container">
-        <div className="logo">Shaik Sameer</div>
-        <ul className="nav-links">
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#experience">Experience</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
+        <div className="logo">Sameer</div>
+        <nav>
+          <ul className="nav-links">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="projects.html">Projects</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
         <div className="mobile-menu">
           <span></span>
           <span></span>
