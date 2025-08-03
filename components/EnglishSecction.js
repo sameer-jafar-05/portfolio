8554240b0b1b1b42eacf469d8b5e72ee5b50d382
sameer-jafar-05 <sameer.jafar.05@gmail.com>
2025-08-03 07:14:34 +0530

    Update EnglishSecction.js

--- components/EnglishSecction.js
+++ components/EnglishSecction.js
@@ -7,7 +7,7 @@ const EnglishSection = () => {
       </div>
 
       <div className="container image-gallery" style={{ display: 'grid', gap: '1rem', gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))' }}>
-        <img src="images/english1.jpg" alt="English Resource 1" style={{ width: '100%', borderRadius: '12px' }} />
+        <img src="images/1753554658027.jpg" alt="English Resource 1" style={{ width: '100%', borderRadius: '12px' }} />
         <img src="images/english2.jpg" alt="English Resource 2" style={{ width: '100%', borderRadius: '12px' }} />
         <img src="images/english3.png" alt="English Resource 3" style={{ width: '100%', borderRadius: '12px' }} />
         {/* Add more images as needed */}
